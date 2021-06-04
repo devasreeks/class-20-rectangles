97df3620bad50b41294fb25decf3fb034d6641f6
devasreeks <84187567+devasreeks@users.noreply.github.com>
2021-06-04 18:46:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var fixedRect;
+var movingRect;
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "pink";
+
+  movingRect = createSprite(400,800,70,50);
+  movingRect.shapeColor = "pink";
+}
+
+function draw() {
+  background(255,255,255);
+
+  
+  
+  movingRect.x = World.mouseX;
+  
+  movingRect.y = World.mouseY;
+
+  if(movingRect.x-fixedRect.x <fixedRect.width/2+movingRect.width/2 && 
+    fixedRect.x- movingRect.x< fixedRect.width/2+movingRect.width/2 &&
+    movingRect.y- fixedRect.y< fixedRect.height/2+movingRect.height/2 &&
+    fixedRect.y- movingRect.y< fixedRect.height/2+movingRect.height/2){
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }
+  else{
+    movingRect.shapeColor = "pink";
+    fixedRect.shapeColor = "pink"; 
+  }
+  drawSprites();
+}
